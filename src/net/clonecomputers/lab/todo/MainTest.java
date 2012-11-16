37652b9b554ecd71e698aff0d1eda5f0d4116f75
1000000000 <lthyde@gmail.com>
2012-11-16 13:24:35 -0500

    made it work for any combination of wildcards and added interactive test
    But please test this more!

--- src/net/clonecomputers/lab/todo/MainTest.java
+++ src/net/clonecomputers/lab/todo/MainTest.java
@@ -4,13 +4,15 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 import org.junit.Test;
 
 public class MainTest {
 	
-	private final Date testDate = new GregorianCalendar(2013, 8, 4, 12, 14).getTime();
+	private final Date testDate = new GregorianCalendar(2012, 11, 4, 12, 14).getTime();
 	private final Date testLeapYearDate = new GregorianCalendar(2016, 1, 29, 12, 14).getTime();
+	private final Scanner in = new Scanner(System.in);
 
 	/*@Test
 	public void test() {
@@ -19,7 +21,7 @@ public class MainTest {
 	
 	@Test
 	public void testDateEquals() {
-		Date alarmDate = new ScheduledAlarm(2013, 8, 4, 12, 14).getSoonestDate();
+		Date alarmDate = new ScheduledAlarm(2012, 11, 4, 12, 14).getSoonestDate();
 		printTestInfo("testDateEquals", alarmDate);
 		assertTrue(testDate.equals(alarmDate));
 	}
@@ -33,7 +35,7 @@ public class MainTest {
 	
 	@Test
 	public void testWildcardYearEqual() {
-		Date alarmDate = new ScheduledAlarm(ScheduledAlarm.WILDCARD, 8, 4, 12, 14).getSoonestDate();
+		Date alarmDate = new ScheduledAlarm(ScheduledAlarm.WILDCARD, 11, 4, 12, 14).getSoonestDate();
 		printTestInfo("testWildcardYearEqual", alarmDate);
 		assertTrue(testDate.equals(alarmDate));
 	}
@@ -43,6 +45,68 @@ public class MainTest {
 		Date alarmDate = new ScheduledAlarm(ScheduledAlarm.WILDCARD, 1, 29, 12, 14).getSoonestDate();
 		printTestInfo("testWildcardYearEqualWithLeapYear", alarmDate, testLeapYearDate);
 		assertTrue(testLeapYearDate.equals(alarmDate));
+	}
+	
+	@Test
+	public void testWildcardMonthEqual() {
+		Date alarmDate = new ScheduledAlarm(2012, ScheduledAlarm.WILDCARD, 4, 12, 14).getSoonestDate();
+		printTestInfo("testWildcardMonthEqual", alarmDate);
+		assertTrue(testDate.equals(alarmDate));
+	}
+	
+	@Test
+	public void testFromUserInput() {
+		System.out.println("Hi World!");
+		String response = "";
+		int aYear = 0;
+		int aMonth = 0;
+		int aDay = 0;
+		int aHour = 0;
+		int aMinute = 0;
+		int eYear = 0;
+		int eMonth = 0;
+		int eDay = 0;
+		int eHour = 0;
+		int eMinute = 0;
+        while(!response.equalsIgnoreCase("y")) {
+        	System.out.println("AlarmTime:");
+        	System.out.println("Input Year:");
+        	aYear = in.nextInt();
+        	System.out.println("Input Month (0-11):");
+        	aMonth = in.nextInt();
+        	System.out.println("Input Day (1-?):");
+        	aDay = in.nextInt();
+        	System.out.println("Input Hour (0-23):");
+        	aHour = in.nextInt();
+        	System.out.println("Input Minute (0-59):");
+        	aMinute = in.nextInt();
+        	System.out.println();
+        	
+        	System.out.println("ExpectedNextTime:");
+        	System.out.println("Input Year:");
+        	eYear = in.nextInt();
+        	System.out.println("Input Month (0-11):");
+        	eMonth = in.nextInt();
+        	System.out.println("Input Day (1-?):");
+        	eDay = in.nextInt();
+        	System.out.println("Input Hour (0-23):");
+        	eHour = in.nextInt();
+        	System.out.println("Input Minute (0-59):");
+        	eMinute = in.nextInt();
+        	
+        	ScheduledAlarm alarm = new ScheduledAlarm(aYear, aMonth, aDay, aHour, aMinute);
+        	long startTime = System.currentTimeMillis();
+        	Date alarmTime = alarm.getSoonestDate();
+        	long elapsedTime = System.currentTimeMillis() - startTime;
+        	Date expectedNextTime = new GregorianCalendar(eYear, eMonth, eDay, eHour, eMinute, 0).getTime();
+        	
+        	printTestInfo("testFromUserInput, time: " + elapsedTime, alarmTime, expectedNextTime);
+        	
+        	assertTrue(expectedNextTime.equals(alarmTime));
+        	
+    		System.out.println("Quit? (y)");
+    		response = in.nextLine();
+        }
 	}
 	
 	private void printTestInfo(String testName, Date alarmDate) {
--- src/net/clonecomputers/lab/todo/ScheduledAlarm.java
+++ src/net/clonecomputers/lab/todo/ScheduledAlarm.java
@@ -1,5 +1,7 @@
 package net.clonecomputers.lab.todo;
 
+import static java.util.Calendar.*;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -29,16 +31,38 @@ public class ScheduledAlarm {
 		Calendar alarmTime = new GregorianCalendar(1,1,1,0,0,0);
 		NOW.setTime(new Date());
 		alarmTime.set(ye,mo,da,ho,mi);
-		if(ye != 60) {
-			return NOW.getTime().before(alarmTime.getTime()) ? alarmTime.getTime() : null;
-		} else {
-			int date = alarmTime.get(Calendar.DATE);
-			alarmTime.set(Calendar.YEAR, NOW.get(Calendar.YEAR));
-			while(!NOW.getTime().before(alarmTime.getTime()) || date > alarmTime.getActualMaximum(Calendar.DATE)) {
-				alarmTime.add(Calendar.YEAR, 1);
-			}
-			alarmTime.set(Calendar.DATE, date);
-			return alarmTime.getTime();
+		if(ye == WILDCARD) alarmTime.set(YEAR, NOW.get(YEAR));
+		if(mo == WILDCARD) alarmTime.set(MONTH, NOW.get(MONTH));
+		if(da == WILDCARD) alarmTime.set(DATE, NOW.get(MONTH));
+		if(ho == WILDCARD) alarmTime.set(HOUR_OF_DAY, NOW.get(HOUR_OF_DAY));
+		if(mi == WILDCARD) {
+			alarmTime.set(MINUTE, NOW.get(MINUTE));
+			alarmTime.add(MINUTE, 1);
 		}
+		if(ho == WILDCARD) {
+			while(!NOW.getTime().before(alarmTime.getTime())) {
+				alarmTime.add(HOUR_OF_DAY, 1);
+			}
+		}
+		if(da == WILDCARD) {
+			while(!NOW.getTime().before(alarmTime.getTime())) {
+				alarmTime.add(DATE, 1);
+			}
+		}
+		if(mo == WILDCARD) {
+			while(!NOW.getTime().before(alarmTime.getTime()) || da > alarmTime.getActualMaximum(DATE)) {
+				alarmTime.add(MONTH, 1);
+			}
+			alarmTime.set(DATE, da);
+		}
+		if(ye != WILDCARD) {
+			if(!NOW.getTime().before(alarmTime.getTime())) return null;
+		} else {
+			while(!NOW.getTime().before(alarmTime.getTime()) || da > alarmTime.getActualMaximum(DATE)) {
+				alarmTime.add(YEAR, 1);
+			}
+			alarmTime.set(DATE, da);
+		}
+		return alarmTime.getTime();
 	}
 }
