b07893a009a1243916574a1d0bff7698a1f92334
1000000000 <lthyde@gmail.com>
2012-11-19 17:02:18 -0500

    Added more convenience methods for testing

--- src/net/clonecomputers/lab/todo/ScheduledAlarm.java
+++ src/net/clonecomputers/lab/todo/ScheduledAlarm.java
@@ -28,41 +28,60 @@ public class ScheduledAlarm {
 	}
 	
 	Date getSoonestDate() {
-		Calendar alarmTime = new GregorianCalendar(1,1,1,0,0,0);
 		NOW.setTime(new Date());
+		return getSoonestDateAfter(NOW);
+	}
+	
+	Date getSoonestDateAfter(Calendar curTime) {
+		Calendar alarmTime = new GregorianCalendar(1,1,1,0,0,0);
 		alarmTime.set(ye,mo,da,ho,mi);
-		if(ye == WILDCARD) alarmTime.set(YEAR, NOW.get(YEAR));
-		if(mo == WILDCARD) alarmTime.set(MONTH, NOW.get(MONTH));
-		if(da == WILDCARD) alarmTime.set(DATE, NOW.get(MONTH));
-		if(ho == WILDCARD) alarmTime.set(HOUR_OF_DAY, NOW.get(HOUR_OF_DAY));
+		if(ye == WILDCARD) alarmTime.set(YEAR, curTime.get(YEAR));
+		if(mo == WILDCARD) alarmTime.set(MONTH, curTime.get(MONTH));
+		if(da == WILDCARD) alarmTime.set(DATE, curTime.get(MONTH));
+		if(ho == WILDCARD) alarmTime.set(HOUR_OF_DAY, curTime.get(HOUR_OF_DAY));
 		if(mi == WILDCARD) {
-			alarmTime.set(MINUTE, NOW.get(MINUTE));
+			alarmTime.set(MINUTE, curTime.get(MINUTE));
 			alarmTime.add(MINUTE, 1);
 		}
 		if(ho == WILDCARD) {
-			while(!NOW.getTime().before(alarmTime.getTime())) {
+			while(!curTime.getTime().before(alarmTime.getTime())) {
 				alarmTime.add(HOUR_OF_DAY, 1);
 			}
 		}
 		if(da == WILDCARD) {
-			while(!NOW.getTime().before(alarmTime.getTime())) {
+			while(!curTime.getTime().before(alarmTime.getTime())) {
 				alarmTime.add(DATE, 1);
 			}
 		}
 		if(mo == WILDCARD) {
-			while(!NOW.getTime().before(alarmTime.getTime()) || da > alarmTime.getActualMaximum(DATE)) {
+			while(!curTime.getTime().before(alarmTime.getTime()) || da > alarmTime.getActualMaximum(DATE)) {
 				alarmTime.add(MONTH, 1);
 			}
 			alarmTime.set(DATE, da);
 		}
 		if(ye != WILDCARD) {
-			if(!NOW.getTime().before(alarmTime.getTime())) return null;
+			if(!curTime.getTime().before(alarmTime.getTime())) return null;
 		} else {
-			while(!NOW.getTime().before(alarmTime.getTime()) || da > alarmTime.getActualMaximum(DATE)) {
+			while(!curTime.getTime().before(alarmTime.getTime()) || da > alarmTime.getActualMaximum(DATE)) {
 				alarmTime.add(YEAR, 1);
 			}
 			alarmTime.set(DATE, da);
 		}
 		return alarmTime.getTime();
+	}
+	
+	Date[] getSoonestDates(int number) {
+		NOW.setTime(new Date());
+		return getSoonestDatesAfter(NOW, number);
+	}
+	
+	Date[] getSoonestDatesAfter(Calendar curTime, int number) {
+		if(number < 1) throw new IllegalArgumentException("for getSoonestDates(), number must at least be one which it is not");
+		Date[] dates = new Date[number];
+		for(int i = 0; i < number; i++) {
+			dates[i] = getSoonestDateAfter(curTime);
+			curTime.setTime(dates[i]);
+		}
+		return dates;
 	}
 }
