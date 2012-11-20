961aea55d2c18c108658a138aafa1c8fc44318f1
1000000000 <lthyde@gmail.com>
2012-11-20 13:03:28 -0500

    Started getting Todo ready for crazy thread and gui stuff

--- src/net/clonecomputers/lab/todo/Todo.java
+++ src/net/clonecomputers/lab/todo/Todo.java
@@ -1,13 +1,40 @@
 package net.clonecomputers.lab.todo;
 
-public class Todo {
+import java.util.Timer;
 
+public class Todo extends Thread {
+
+	private final Timer timer;
+	
+	private boolean guiOpen;
+	
+	private static Todo running;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if(running == null){
+			running = new Todo();
+			running.start();
+		}
+		running.joinOpenGui();
+	}
+	
+	private Todo() {
+		timer = new Timer("todo-timer-thread");
+	}
 
+	@Override
+	public void run() {
+		
+	}
+	
+	private void joinOpenGui() {
+		guiOpen = true;
+		while(guiOpen) {
+			this.yield();
+		}
 	}
 
 }
