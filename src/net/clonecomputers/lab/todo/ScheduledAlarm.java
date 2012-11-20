8b05dee37d29d1e13c4acb6d54d8093c1fc3b259
1000000000 <bjoeyjoe@gmail.com>
2012-11-20 12:55:50 -0500

    Moved MainTest inside of ScheduledAlarm and privated a bunch of methods

--- src/net/clonecomputers/lab/todo/ScheduledAlarm.java
+++ src/net/clonecomputers/lab/todo/ScheduledAlarm.java
@@ -1,10 +1,15 @@
 package net.clonecomputers.lab.todo;
 
 import static java.util.Calendar.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+import org.junit.Test;
 
 public class ScheduledAlarm {
 
@@ -27,12 +32,12 @@ public class ScheduledAlarm {
 		mi = minute;
 	}
 	
-	Date getSoonestDate() {
+	private Date getSoonestDate() {
 		NOW.setTime(new Date());
 		return getSoonestDateAfter(NOW);
 	}
 	
-	Date getSoonestDateAfter(Calendar curTime) {
+	private Date getSoonestDateAfter(Calendar curTime) {
 		Calendar alarmTime = new GregorianCalendar(1,1,1,0,0,0);
 		alarmTime.set(ye,mo,da,ho,mi);
 		if(ye == WILDCARD) alarmTime.set(YEAR, curTime.get(YEAR));
@@ -70,12 +75,12 @@ public class ScheduledAlarm {
 		return alarmTime.getTime();
 	}
 	
-	Date[] getSoonestDates(int number) {
+	private Date[] getSoonestDates(int number) {
 		NOW.setTime(new Date());
 		return getSoonestDatesAfter(NOW, number);
 	}
 	
-	Date[] getSoonestDatesAfter(Calendar curTime, int number) {
+	private Date[] getSoonestDatesAfter(Calendar curTime, int number) {
 		if(number < 1) throw new IllegalArgumentException("for getSoonestDates(), number must at least be one which it is not");
 		Date[] dates = new Date[number];
 		for(int i = 0; i < number; i++) {
@@ -84,4 +89,126 @@ public class ScheduledAlarm {
 		}
 		return dates;
 	}
+	
+	public class AlarmTest {
+		
+		private final Date testDate = new GregorianCalendar(2012, 11, 4, 12, 14).getTime();
+		private final Date testLeapYearDate = new GregorianCalendar(2016, 1, 29, 12, 14).getTime();
+		private final Scanner in = new Scanner(System.in);
+
+		/*@Test
+		public void test() {
+			assertTrue(false);
+		}*/
+		
+		@Test
+		public void testDateEquals() {
+			Date alarmDate = new ScheduledAlarm(2012, 11, 4, 12, 14).getSoonestDate();
+			printTestInfo("testDateEquals", alarmDate);
+			assertTrue(testDate.equals(alarmDate));
+		}
+		
+		@Test
+		public void testDateNotEqual() {
+			Date alarmDate = new ScheduledAlarm(2013, 1, 1, 0, 0).getSoonestDate();
+			printTestInfo("testDateNotEqual", alarmDate);
+			assertFalse(testDate.equals(alarmDate));
+		}
+		
+		@Test
+		public void testWildcardYearEqual() {
+			Date alarmDate = new ScheduledAlarm(ScheduledAlarm.WILDCARD, 11, 4, 12, 14).getSoonestDate();
+			printTestInfo("testWildcardYearEqual", alarmDate);
+			assertTrue(testDate.equals(alarmDate));
+		}
+		
+		@Test
+		public void testWildcardYearEqualWithLeapYear() {
+			Date alarmDate = new ScheduledAlarm(ScheduledAlarm.WILDCARD, 1, 29, 12, 14).getSoonestDate();
+			printTestInfo("testWildcardYearEqualWithLeapYear", alarmDate, testLeapYearDate);
+			assertTrue(testLeapYearDate.equals(alarmDate));
+		}
+		
+		@Test
+		public void testWildcardMonthEqual() {
+			Date alarmDate = new ScheduledAlarm(2012, ScheduledAlarm.WILDCARD, 4, 12, 14).getSoonestDate();
+			printTestInfo("testWildcardMonthEqual", alarmDate);
+			assertTrue(testDate.equals(alarmDate));
+		}
+		
+		@Test
+		public void testFromUserInput() {
+			System.out.println("Hi World!");
+			String response = "";
+			int aYear = 0;
+			int aMonth = 0;
+			int aDay = 0;
+			int aHour = 0;
+			int aMinute = 0;
+			int eYear = 0;
+			int eMonth = 0;
+			int eDay = 0;
+			int eHour = 0;
+			int eMinute = 0;
+	        while(!response.equalsIgnoreCase("y")) {
+	        	System.out.println("AlarmTime:");
+	        	System.out.println("Input Year:");
+	        	aYear = in.nextInt();
+	        	System.out.println("Input Month (0-11):");
+	        	aMonth = in.nextInt();
+	        	System.out.println("Input Day (1-?):");
+	        	aDay = in.nextInt();
+	        	System.out.println("Input Hour (0-23):");
+	        	aHour = in.nextInt();
+	        	System.out.println("Input Minute (0-59):");
+	        	aMinute = in.nextInt();
+	        	System.out.println();
+	        	
+	        	System.out.println("ExpectedNextTime:");
+	        	System.out.println("Input Year:");
+	        	eYear = in.nextInt();
+	        	System.out.println("Input Month (0-11):");
+	        	eMonth = in.nextInt();
+	        	System.out.println("Input Day (1-?):");
+	        	eDay = in.nextInt();
+	        	System.out.println("Input Hour (0-23):");
+	        	eHour = in.nextInt();
+	        	System.out.println("Input Minute (0-59):");
+	        	eMinute = in.nextInt();
+	        	
+	        	ScheduledAlarm alarm = new ScheduledAlarm(aYear, aMonth, aDay, aHour, aMinute);
+	        	long startTime = System.currentTimeMillis();
+	        	Date alarmTime = alarm.getSoonestDate();
+	        	long elapsedTime = System.currentTimeMillis() - startTime;
+	        	Date expectedNextTime = new GregorianCalendar(eYear, eMonth, eDay, eHour, eMinute, 0).getTime();
+	        	
+	        	printTestInfo("testFromUserInput, time: " + elapsedTime, alarmTime, expectedNextTime);
+	        	
+	        	assertTrue(expectedNextTime.equals(alarmTime));
+	        	
+	    		System.out.println("Quit? (y)");
+	    		response = in.nextLine();
+	        }
+		}
+		
+		private void printTestInfo(String testName, Date alarmDate) {
+			System.out.println("-------------------------------------------");
+			System.out.println("Test: " + testName);
+			System.out.println("    Test Date:  " + testDate);
+			System.out.println("    Alarm Date: " + alarmDate);
+			System.out.println("    Equal?:     " + testDate.equals(alarmDate));
+			System.out.println("-------------------------------------------");
+		}
+		
+		private void printTestInfo(String testName, Date alarmDate, Date theTestDate) {
+			System.out.println("-------------------------------------------");
+			System.out.println("Test: " + testName);
+			System.out.println("    Test Date:  " + theTestDate);
+			System.out.println("    Alarm Date: " + alarmDate);
+			System.out.println("    Equal?:     " + theTestDate.equals(alarmDate));
+			System.out.println("-------------------------------------------");
+		}
+
+	}
+
 }
