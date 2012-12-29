d7501a00af4ef57028a4d60876026938cf0d1cf2
giobkboy <giobkboy@gmail.com>
2012-12-29 16:35:14 -0500

    Removed all the stuff using Timer, started on gui
    I heard about cron and since java is inefficient I decided to get
    rid of all the Timer stuff and use cron instead

--- src/net/clonecomputers/lab/todo/Todo.java
+++ src/net/clonecomputers/lab/todo/Todo.java
@@ -1,40 +1,49 @@
 package net.clonecomputers.lab.todo;
 
-import java.util.Timer;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Todo extends Thread {
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	private final Timer timer;
-	
-	private boolean guiOpen;
-	
-	private static Todo running;
+public class Todo {
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if(running == null){
-			running = new Todo();
-			running.start();
-		}
-		running.joinOpenGui();
-	}
-	
-	private Todo() {
-		timer = new Timer("todo-timer-thread");
-	}
+		EventQueue.invokeLater(new Runnable() {
 
-	@Override
-	public void run() {
-		
+			@Override
+			public void run() {
+				initGui();
+			}
+			
+		});
 	}
 	
-	private void joinOpenGui() {
-		guiOpen = true;
-		while(guiOpen) {
-			this.yield();
-		}
+	private static void initGui() {
+		JFrame window = new JFrame("clone todo");
+		JPanel contentPane = new JPanel(new BorderLayout(30, 0));
+		contentPane.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+		contentPane.add(getAlarmListPanel(), BorderLayout.WEST);
+		contentPane.add(getAlarmSettingsPanel(), BorderLayout.EAST);
+		window.setContentPane(contentPane);
+		window.pack();
+		window.setVisible(true);
+	}
+	
+	private static JPanel getAlarmListPanel() {
+		JPanel list = new JPanel(new BorderLayout());
+		//TODO create alarm list panel
+		return list;
+	}
+	
+	private static JPanel getAlarmSettingsPanel() {
+		JPanel settings = new JPanel(/*Layout to be determined*/);
+		//TODO make alarm settings panel
+		return settings;
 	}
 
 }
