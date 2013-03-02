215b3bb31884f37e29391cf8a81a841ebeeeae58
1000000000 <lthyde@gmail.com>
2013-03-02 10:52:10 -0500

    Worked on ListCellRenderer for the list of alarms

--- src/net/clonecomputers/lab/todo/Todo.java
+++ src/net/clonecomputers/lab/todo/Todo.java
@@ -4,8 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
@@ -59,14 +63,7 @@ public class Todo {
 		alarmList = new JList(alarms);
 		alarmList.setFixedCellHeight(80);
 		alarmList.setFixedCellWidth(350);
-		alarmList.setCellRenderer(new ListCellRenderer() {
-			
-			public Component getListCellRendererComponent(JList list, Object value,
-					int index, boolean isSelected, boolean cellHasFocus) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-		});
+		alarmList.setCellRenderer(new AlarmListCellRenderer());
 		alarmList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		alarmList.setDragEnabled(false);
 		alarmList.clearSelection();
@@ -115,6 +112,44 @@ public class Todo {
 	
 	private static void removeSelectedAlarm() {
 		//TODO write this
+	}
+	
+	public static class AlarmListCellRenderer extends Component implements ListCellRenderer {
+		
+		private static final long serialVersionUID = -323811720627355823L;
+		
+		private Image img;
+		private Image selImg;
+		
+		private boolean isSelected;
+		
+		private ScheduledAlarm cellValue = null;
+		
+		public AlarmListCellRenderer() {
+			img = Toolkit.getDefaultToolkit().createImage("resources/Louis/ListItemBackground.jpg");
+			selImg = Toolkit.getDefaultToolkit().createImage("resources/Louis/SelectedListItemBackground.jpg");
+		}
+		
+		public Component getListCellRendererComponent(JList list, Object value,
+				int index, boolean isSelected, boolean cellHasFocus) {
+			this.isSelected = isSelected;
+			if(value instanceof ScheduledAlarm) {
+				cellValue = (ScheduledAlarm)value;
+			}
+			return this;
+		}
+		
+		@Override
+		public void paint(Graphics g) {
+			if(isSelected) {
+				g.drawImage(selImg, 0, 0, null);
+			} else {
+				g.drawImage(img, 0, 0, null);
+			}
+			if(cellValue == null) return;
+			//TODO draw rest of list component
+		}
+		
 	}
 
 }
